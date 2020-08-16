1d353c6ba8d998d0a05efb4d70e69fa2f9f76b0d
virginie <virginiegodfrin@gmail.com>
2020-08-16 14:33:11 +0200

    Chap 19: the single Runtime Chunk

--- assets/js/article_show.js
+++ assets/js/article_show.js
@@ -1,7 +1,9 @@
 import '../css/article_show.scss';
 import $ from 'jquery';
+//import 'bootstrap';
 
 $(document).ready(function() {
+    $('.js-like-article').tooltip();
     $('.js-like-article').on('click', function(e) {
         e.preventDefault();
 
