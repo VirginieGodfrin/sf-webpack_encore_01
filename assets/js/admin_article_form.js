890e7bac615784904a0c881e0f4226a885a9901b
virginie <virginiegodfrin@gmail.com>
2020-08-16 15:08:48 +0200

    Chap20 Async Import

--- assets/js/admin_article_form.js
+++ assets/js/admin_article_form.js
@@ -2,15 +2,16 @@ import $ from 'jquery';
 import Dropzone from 'dropzone';
 import 'dropzone/dist/dropzone.css';
 import Sortable from 'sortablejs';
-import './components/algolia-autocomplete';
-import autocomplete from './components/algolia-autocomplete';
+
 
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
     const $autoComplete = $('.js-user-autocomplete');
     if(!$autoComplete.is(':disabled')){
-        autocomplete($autoComplete, 'users', 'email');
+        import ('./components/algolia-autocomplete').then((autocomplete) => {
+            autocomplete.default($autoComplete, 'users', 'email');
+        });
     }
     const $referenceList = $('.js-reference-list');
     if ($referenceList[0]) {
